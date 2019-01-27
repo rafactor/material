7510e5e284a863b1584e28a58f08605cde02f5ca
rafactor <avila.rafael@gmail.com>
2019-01-27 15:27:35 -0500

    basic material design

---
+++ server.js
@@ -0,0 +1,35 @@
+console.log('server')
+
+// *** Dependencies
+// =============================================================
+// var express = require("express");
+// var htmlRoutes = require('./routes/html-routes');
+
+
+// Packages set up
+// =============================================================
+// var app = express();
+// var router = express.Router();
+
+// Requiring our models for syncing
+// var db = require("./models");
+
+// Sets up the Express app to handle data parsing
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// Set Handlebars.
+// var exphbs = require("express-handlebars");
+
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+
+// Static directory
+// app.use(express.static("public"));
+
+// Routes
+// =============================================================
+// app.use('/',htmlRoutes)
+// app.use('/api',apis)
+
+// db.sequelize.sync()
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  entry: ['./sass/app.scss', './app.js','./server.js'],
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'bundle.css',
+            },
+          },
+          {loader: 'extract-loader'},
+          {loader: 'css-loader'},
+          {loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer()],
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: ['./node_modules'],
+            },
+          }
+        ],
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+          plugins: ['transform-object-assign']
+        },
+      }
+    ],
+  },
+};
